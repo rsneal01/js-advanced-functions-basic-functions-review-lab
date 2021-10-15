ffc7d644ec4f4cffecbcb144818c9c46e6758591
rsneal01 <rsneal01@gmail.com>
2021-10-15 10:39:30 -0700

    commit tree daa07b8a22d0880351c110b66898fe8a65241a49

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork(activity = "go to the office"){
+    return (`This Monday, I will ${activity}.`);
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adj = "special"){
+        return `You are ${flair}${adj}${flair}!`;
+    }
+}
+
+ const Calculator = {
+     add(num1, num2) {
+        return num1 + num2;
+     },
+     subtract(num1, num2) {
+        return num1 - num2;
+     },
+     multiply(num1, num2){
+        return num1 * num2;
+     },
+     divide(num1, num2){
+        return num1 / num2;
+     }
+ }
+
+ let actionApplyer = function(start, ray) {
+    let a = start
+  
+    for (let i = 0; i < ray.length; i++ ){
+      a = ray[i](a)
+    }
+  
+    return a
+  }
+
+
